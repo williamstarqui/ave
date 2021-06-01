2bf32798f54318878ac8ffea589c0f3d6e9ca7a3
williamstarqui <williamstarquisilva@gmail.com>
2021-06-01 12:39:23 -0400

    config

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://ave-2vouq.ondigitalocean.app/'),
 
     'asset_url' => env('ASSET_URL', null),
 
